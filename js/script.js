cc6b0da1a9ad75168aa7e70c52628aa78355578c
Serezha <volos11223344@yandex.ru>
2023-01-05 21:50:46 +0300

    new comm

--- js/script.js
+++ js/script.js
@@ -24,34 +24,83 @@ const movieDB = {
 	]
 };
 
-const promoImg = document.querySelectorAll('.promo__adv img'),
-	poster = document.querySelector('.promo__bg'),
-	genre = poster.querySelector('.promo__genre'),
-	movieList = document.querySelector('.promo__interactive-list');
+const imgs = document.querySelectorAll('.promo__adv img'),
+      genre = document.querySelector('.promo__genre'),
+      promoBG = document.querySelector('.promo__bg');
+      
 
-//1 1) Удалить все рекламные блоки со страницы (правая часть сайта)
-promoImg.forEach(function(img) {
+//1) Удалить все рекламные блоки со страницы (правая часть сайта)
+imgs.forEach((img) => {
+  console.log(img);
   img.remove();
 });
 
-//2 Изменить жанр фильма, поменять "комедия" на "драма"
+//2) Изменить жанр фильма, поменять "комедия" на "драма"
 genre.textContent = 'Драма';
 
-//3 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+//3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+//Реализовать только при помощи JS
+
+promoBG.style.backgroundImage = 'url(img/bg.jpg)';
 
 //4 Список фильмов на странице сформировать на основании данных из этого JS файла.
-//Отсортировать их по алфавиту
+Отсортировать их по алфавиту 
 
-movieList.innerHTML = ''; //!Удаляем список полностью(очищаем его)
-movieDB.movies.sort();    //!Сортируем элементы по алфавиту
 
-movieDB.movies.forEach((film, i) => {
-  movieList.innerHTML += `
-	<li class="promo__interactive-item">${i + 1} ${film}
-		<div class="delete"></div>
-	</li>
-	`;
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const promoImg = document.querySelectorAll('.promo__adv img'),
+// 	poster = document.querySelector('.promo__bg'),
+// 	genre = poster.querySelector('.promo__genre'),
+// 	movieList = document.querySelector('.promo__interactive-list');
+
+// //1 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+// promoImg.forEach(function(img) {
+//   img.remove();
+// });
+
+// //2 Изменить жанр фильма, поменять "комедия" на "драма"
+// genre.textContent = 'Драма';
+
+// //3 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+// poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// //4 Список фильмов на странице сформировать на основании данных из этого JS файла.
+// //Отсортировать их по алфавиту
+
+// movieList.innerHTML = ''; //!Удаляем список полностью(очищаем его)
+// movieDB.movies.sort();    //!Сортируем элементы по алфавиту
+
+// movieDB.movies.forEach((film, i) => {
+//   movieList.innerHTML += `
+// 	<li class="promo__interactive-item">${i + 1} ${film}
+// 		<div class="delete"></div>
+// 	</li>
+// 	`;
+// });
 
 
